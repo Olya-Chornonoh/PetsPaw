08b2d09849c0f1138080a398de30d02971ec1f15
Olha Chornonoh <olyalarkaa@gmail.com>
2022-07-22 21:18:13 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<style scoped></style>
